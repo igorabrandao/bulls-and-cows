1430a0e5264f0c615412336a3f210757449f63b5
Igor Brandão <igorabrandao@gmail.com>
2021-08-26 21:08:51 -0300

    Added the user input handler

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,39 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Setting up the game initial state
+    this->SetupGame();
+
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the %i letter word!"), this->HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and press enter to continue..."));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (Input == this->HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        if (Input.Len() != this->HiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is %i characters long, try again!"), this->HiddenWord.Len());
+        }
+
+        PrintLine(TEXT("You have Lost!"));
+    }
+}
+
+/**
+ * Method to handle the game initialization
+ */
+void UBullCowCartridge::SetupGame()
+{
+    this->HiddenWord = TEXT("cakes");
+    this->Lives = 4;
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,16 +6,18 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
+	virtual void OnInput(const FString &Input) override;
+	void SetupGame();
 
 	// Your declarations go below!
-	private:
-	
+private:
+	FString HiddenWord;
+	int32 Lives;
 };
